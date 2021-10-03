4dcb3fdb4daa632e98c747111de68956a4d691f5
Vikrant Reddy <vikrantreddy@hotmail.com>
2021-10-03 16:19:18 +0530

    Added pricing section

---
+++ assets/js/overlay.js
@@ -0,0 +1,16 @@
+function addOverlay() {
+  document.getElementById("overlay").style.opacity = 1;
+  document.getElementById("overlay").style.visibility = "visible";
+}
+
+function removeOverlay() {
+  document.getElementById("overlay").style.opacity = 0;
+  document.getElementById("overlay").style.visibility = "hidden";
+}
+
+document.getElementById("service-1").addEventListener("click", addOverlay);
+document.getElementById("service-2").addEventListener("click", addOverlay);
+document.getElementById("service-3").addEventListener("click", addOverlay);
+document.getElementById("service-4").addEventListener("click", addOverlay);
+
+document.getElementById("overlay").addEventListener("click", removeOverlay);
